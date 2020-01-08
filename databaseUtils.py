8141c22c136ac90bc070917c559addafe3a71234
athompson00 <athompson00@stuy.edu>
2020-01-07 21:12:43 -0500

    created skeleton files

---
+++ databaseUtils.py
@@ -0,0 +1,26 @@
+def addUser(user, pass, startingP):
+    #adds user and password to the database
+    #also adds user's starting pokemon
+    #creates table if it doesn't exist
+    return 0
+
+def getUser(user):
+    #gets stored user info like their pokemon and returns it
+    userInfo = {}
+    return userInfo
+
+def addPokemon(name):
+    #adds specified pokemon to the user's list
+    return 0
+
+def getBackground(name):
+    #gets specified background image to be used in the battle
+    imageURL = ""
+    return imageURL
+
+def getRandTeam(level):
+    #randomly gets a full team of pokemon to battle against
+    #if we have time add level so that we sometimes filter to stronger pokemon
+    team = {}
+    return team
+    
