57ba755f413c767cda6da68074a6a08b34b56b71
athompson00 <athompson00@stuy.edu>
2020-01-16 03:31:06 -0500

    trying to make build

--- static/build.js
+++ static/build.js
@@ -1,20 +1,28 @@
 var moves = 0;
 var pokemon = JSON.parse(document.getElementById('allPokemonJson').name);
-console.log(pokemon);
+//console.log(pokemon);
 
 
 var pokemonButton = document.getElementById("pokemonButton");
 pokemonButton.addEventListener("click", loadPokemon);
+console.log(pokemonButton)
 
-var moveButton = document.getElementById("moveButton");
-moveButton.addEventListener("click", loadMove);
+var pokemonDropdown = document.getElementById("pokemonDropdown");
+console.log(pokemonDropdown.value);
 
-var finishPokemon = document.getElementById("finishPokemon");
-finishPokemon.addEventListener("click", finishPokemon);
+// var moveButton = document.getElementById("moveButton");
+// moveButton.addEventListener("click", loadMove);
+
+// var finishPokemon = document.getElementById("finishPokemon");
+// finishPokemon.addEventListener("click", finishPokemon);
 
 var loadPokemon = function(e) {
     var image = document.getElementById("pdisplay");
     image.src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png";
+}
+
+var loadPokemon = function(e){
+  console.log(e);
 }
 
 var loadMove = function(e) {
