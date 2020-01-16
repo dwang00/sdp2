25d7e99eb404ae1d2ba15d064349c68b2dd09774
jchen9182 <jchen9182@gmail.com>
2020-01-16 00:22:11 -0500

    build stuff

--- static/build.js
+++ static/build.js
@@ -1,13 +1,16 @@
 var moves = 0;
+var pokemon = JSON.parse();
+var moves = JSON.parse();
 
 var button = document.getElementById("b1");
 button.addEventListener("click", loadPokemon);
 
 var button2 = document.getElementById("b2");
-button.addEventListener("click", loadPokemon);
+button.addEventListener("click", loadMove);
 
 var loadPokemon = function(e) {
-
+    var image = document.getElementById("pdisplay");
+    image.src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png";
 }
 
 var loadMove = function(e) {
